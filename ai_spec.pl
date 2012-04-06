6014713000810655e956b1c60c6cf817e49ff2f9
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-04-06 09:20:05 -0500

    ask it to look farther into future

--- ai_spec.pl
+++ ai_spec.pl
@@ -4,38 +4,56 @@
 :- use_module(ttt_spec).
 :- begin_tests(ai).
 
-test(score_world) :-
+test(score_world,
+     [cleanup(ttt_spec:clear_moves)]) :-
   ttt_spec:assert_row(0,x),
   score_world(1,x),
-  \+score_world(0,x),
+  \+score_world(0,x).
 /*  findall(Score, score_world(Score), Scores),
   assertion(Scores=[1]),*/
-  ttt_spec:clear_moves.
 
-test(score_world) :-
+test(score_world,
+     [cleanup(ttt_spec:clear_moves)]) :-
   ttt_spec:assert_row(0,o),
   findall(Score, score_world(Score,o), Scores),
-  assertion(Scores=[-1]),
-  ttt_spec:clear_moves.
+  assertion(Scores=[-1]).
 
 test(score_world) :-
   findall(Score, score_world(Score,x), Scores),
   assertion(Scores=[0]).
 
 
-test(imminent_victories) :-
+test(imminent_victories, 
+     [cleanup(ttt_spec:clear_moves)]) :-
   ttt:assert(move(point(0,0),o)),
   ttt:assert(move(point(0,1),o)),
   findall(Score, score_world(Score,o), Scores),
-  write(Scores),
   assertion(Scores=[-1]).
 
-test(imminent_victories) :-
+test(imminent_victories,
+     [cleanup(ttt_spec:clear_moves)]) :-
   ttt:assert(move(point(0,0),x)),
   ttt:assert(move(point(0,1),x)),
   findall(Score, score_world(Score,x), Scores),
-  write(Scores),
   assertion(Scores=[1]).
+
+test(distant_victories,
+     [cleanup(ttt_spec:clear_moves)]) :-
+  ttt:assert(move(point(0,0),x)),
+  ttt:assert(move(point(0,2),x)),
+  ttt:assert(move(point(1,1),o)),
+  findall(Score, score_world(Score,o), Scores),
+  assertion(Scores=[1]).
+  
+
+
+
+
+
+
+
+
+
 
 
 :- end_tests(ai).
